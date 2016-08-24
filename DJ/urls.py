0671040d774f30963a190affeefa68d3c16d124d
Pechenin.NA <chpnick1@gmail.com>
2016-08-25 00:06:38 +0300

    add new def in views, add new file

--- DJ/urls.py
+++ DJ/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-admin.autodiscover()
+# admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^blog/', include('blog.urls')),
 ]
